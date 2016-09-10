a03d65758a1d68d6df99814881781cbd2c8df6e0
aekkang <akang@caltech.edu>
2016-09-09 23:16:51 -0700

    Gonna try more Bootstrap stuff

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+   var location = window.location;
+   var found = false;
+   $("#sidebar-content a").each(function() {
+      var href = $(this).attr("href");
+      if(href == location) {
+         $(this).parent().addClass("selected");
+         found = true;
+      }
+   });
+   if(!found){
+      $("#sidebar-content li:first").addClass("selected");
+   }
+});
