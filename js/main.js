886d214660c58a8730a7d1c68ebbe53e4f6b4de3
aekkang <akang@caltech.edu>
2016-09-10 21:12:41 -0700

    Added fade-in effect

--- js/main.js
+++ js/main.js
@@ -15,9 +15,29 @@ $(document).ready(function () {
 
 /* TODO: loading icon*/
 $(function() {
-    $('#wrapper').hide();
+    $("#wrapper").hide();
 });
 
 $(window).load(function() {
     $("#wrapper").show();
 });
+
+/*
+setInterval(function () {
+    $("#main-fade").fadeToggle();
+
+}, 3000);
+*/
+
+i=0
+var words = ["Programmer", "Pianist", "Mathematician", "Canadian"];
+
+function fade() {
+    $('#main-fade').fadeIn(500).delay(2000).html(words[i % words.length]).fadeOut(500);
+    i++;  //counter, set to 0 initially
+
+    setTimeout(fade, 3000);
+}
+
+fade();
+
