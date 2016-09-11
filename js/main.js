977716be910fbd46a52d7d60625f7ce19778e2e9
aekkang <akang@caltech.edu>
2016-09-11 15:55:39 -0700

    Fixed word transitions - thanks Noah :D

--- js/main.js
+++ js/main.js
@@ -31,16 +31,51 @@ setInterval(function () {
 }, 3000);
 */
 
+// TODO: use CSS3 transitions and Transit plugin
+// one possible solution: use four different divs with CSS3 animations
+/*
 i=0
 var words = ["Programmer", "Pianist", "Mathematician", "Canadian"];
 
-// TODO: use CSS3 transitions and Transit plugin
 function fade() {
     $('#main-fade').fadeIn(1000).delay(2000).html(words[i % words.length]).fadeOut(1000);
     i++;  //counter, set to 0 initially
 
     setTimeout(fade, 4000);
 }
+*/
 
+// Adapted from http://jsfiddle.net/e4sb6tn0/#
+(function() {
+
+    var words = $(".main-fade");
+    var wordIndex = -1;
+    
+    function showNextWord() {
+        ++wordIndex;
+        words.eq(wordIndex % words.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextWord);
+    }
+    
+    showNextWord();
+    
+})();
+
+/*
 fade();
 
+function one() {
+    $("#main-fade").fadeIn(1000);
+    $("#main-fade").delay(2000);
+    $("#main-fade").fadeOut(1000, two);
+}
+
+function two() {
+    $("#world").fadeIn(1000);
+  $("#world").fadeOut(1000, one);
+}
+
+one()
+*/
