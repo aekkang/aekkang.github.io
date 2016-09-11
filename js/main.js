7742c3716cacd4961e098ce2b3835af67cc7d50c
aekkang <akang@caltech.edu>
2016-09-10 18:20:19 -0700

    Attempting loading functions

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
-   var location = window.location;
-   var found = false;
-   $("#sidebar-content a").each(function() {
-      var href = $(this).attr("href");
-      if(href == location) {
-         $(this).parent().addClass("selected");
-         found = true;
-      }
-   });
-   if(!found){
-      $("#sidebar-content li:first").addClass("selected");
-   }
+    var location = window.location;
+    var found = false;
+    $("#sidebar-content a").each(function() {
+        var href = $(this).attr("href");
+        if(href == location) {
+            $(this).parent().addClass("selected");
+            found = true;
+        }
+    });
+    if(!found){
+        $("#sidebar-content li:first").addClass("selected");
+    }
+});
+
+$(function() {
+    $('#wrapper').hide();
+});
+
+$(window).load(function() {
+    $("#wrapper").show();
 });
